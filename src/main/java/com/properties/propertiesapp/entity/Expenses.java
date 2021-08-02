51e7e735c0fb1e4face505c813cac9073262933f
Dave <davidnjau21@gmail.com>
2021-08-02 13:52:15 +0300

    add expenses table

---
+++ src/main/java/com/properties/propertiesapp/entity/Expenses.java
@@ -0,0 +1,121 @@
+package com.properties.propertiesapp.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Expenses {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(
+                            name = "uui_gen_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+                    )
+            }
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    private String propertyId;
+
+    private String expenseName;
+
+    private String expenseAmount;
+    
+    private Date expenseDate;
+
+    private String expenseType;
+
+    public Expenses() {
+    }
+
+    public Expenses(String propertyId, String expenseName, String expenseAmount, Date expenseDate, String expenseType) {
+        this.propertyId = propertyId;
+        this.expenseName = expenseName;
+        this.expenseAmount = expenseAmount;
+        this.expenseDate = expenseDate;
+        this.expenseType = expenseType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(String propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getExpenseName() {
+        return expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public String getExpenseAmount() {
+        return expenseAmount;
+    }
+
+    public void setExpenseAmount(String expenseAmount) {
+        this.expenseAmount = expenseAmount;
+    }
+
+    public Date getExpenseDate() {
+        return expenseDate;
+    }
+
+    public void setExpenseDate(Date expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+
+    public String getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(String expenseType) {
+        this.expenseType = expenseType;
+    }
+}
--- src/main/java/com/properties/propertiesapp/helper_class/Notification.kt
+++ src/main/java/com/properties/propertiesapp/helper_class/Notification.kt
@@ -3,3 +3,7 @@ package com.properties.propertiesapp.helper_class
 enum class Notification () {
     PENDING, NOTIFICATION, COMPLETED, OVERDUE, UNDERPAID
 }
+
+enum class Expense(){
+    UTILITIES, EXTRA
+}
