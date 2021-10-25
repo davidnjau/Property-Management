125e50768c62ee6777880384993f897bb3e5e1ac
Dave <davidnjau21@gmail.com>
2021-10-25 18:39:30 +0300

    logout

--- src/main/java/com/properties/propertiesapp/authentication/controller/StaffDetailsController.java
+++ src/main/java/com/properties/propertiesapp/authentication/controller/StaffDetailsController.java
@@ -8,8 +8,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -66,6 +71,14 @@ public class StaffDetailsController {
 
     }
 
+    @RequestMapping(value="/logout", method=RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
 
 
 }
